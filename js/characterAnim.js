e5aedcc1599ec5727587488aacb82ecc97d2c7db
sarahtalbot <sarahetalbot@gmail.com>
2018-04-28 12:29:31 -0400

    Arrow animation, character loading
    updates to index.js for animation

--- js/characterAnim.js
+++ js/characterAnim.js
@@ -1,21 +1,6 @@
 
-// const animationContainer = document.getElementByClassName("lottie");
 
-// const animData = {
-//   container: animationContainer,
-//   renderer: "svg",
-//   loop: true,
-//   autoplay: true,
-//   // rendererSettings: {
-//   //   preserveAspectRatio: "xMidYMid meet",
-//   // },
-//   path: "js/json/data-idle.json",
-// };
-
-// anim = lottie.loadAnimation(animData);
-// lottie_api.createAnimationApi(anim);
-
-
+// -----------------------------------------
 const animation = bodymovin.loadAnimation({
   container: document.querySelector('.bodymovin'),
   renderer: 'svg',
@@ -23,3 +8,80 @@ const animation = bodymovin.loadAnimation({
   autoplay: true,
   path: "js/json/data-idle.json",
 })
+
+
+const rightArrowAnim = bodymovin.loadAnimation({
+	container: document.querySelector('.js-slick-next'),
+  	renderer: 'svg',
+  	loop: false,
+  	autoplay: false,
+  	prerender:false,
+  	path: "js/json/data-navigationArrow-right.json",
+})
+
+const leftArrowAnim = bodymovin.loadAnimation({
+	container: document.querySelector('.js-slick-prev'),
+  	renderer: 'svg',
+  	loop: false,
+  	autoplay: false,
+  	prerender:false,
+  	path: "js/json/data-navigationArrow-left.json",
+})
+
+
+
+// -----------------------------------------------------------------
+
+// const rollOutArrow = (arrow) =>{
+// 	arrow.playSegments([1,12]);
+// }
+
+// const rollInArrow = (arrow) =>{
+// 	arrow.playSegments([13,24]);
+// }
+
+// -----------------------------------------------------
+
+let isMousedOverRight = false;
+rightArrow = document.querySelector('.js-slick-next');
+rightArrow.addEventListener('mouseenter', ()=>{
+	console.log("mouseouver arrow")
+	// if(isMousedOverRight){
+	// 	return;
+	// }
+	// rollOutArrow(rightArrow);
+	rightArrowAnim.playSegments([1,12]);
+	isMousedOverRight = true;
+})
+rightArrow.addEventListener('mouseout', ()=>{
+	console.log("mouse out arrow")
+	// rollInArrow(rightArrow);
+	rightArrowAnim.playSegments([13,24]);
+	isMousedOverRight = false;
+})
+
+let isMousedOverLeft = false;
+leftArrow = document.querySelector('.js-slick-prev');
+leftArrow.addEventListener('mouseenter', ()=>{
+	console.log("mouseover arrow")
+	// if(isMousedOverLeft){
+	// 	return;
+	// }
+	// rollOutArrow(leftArrow);
+	leftArrowAnim.playSegments([1,12])
+	isMousedOverLeft = true;
+})
+leftArrow.addEventListener('mouseout', ()=>{
+	console.log("mouse out arrow")
+	// rollInArrow(leftArrow);
+	leftArrowAnim.playSegments([13,24]);
+	isMousedOverLeft = false;
+})
+
+
+
+
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -1,3 +1,98 @@
+// JS for ApataSCOREus
+
+
+
+// Arrow Buttons
+const rightArrowAnim = bodymovin.loadAnimation({
+	container: document.querySelector('.js-slick-next'),
+  	renderer: 'svg',
+  	loop: false,
+  	autoplay: false,
+  	prerender:false,
+  	path: "js/json/data-navigationArrow-right.json",
+})
+
+const leftArrowAnim = bodymovin.loadAnimation({
+	container: document.querySelector('.js-slick-prev'),
+  	renderer: 'svg',
+  	loop: false,
+  	autoplay: false,
+  	prerender:false,
+  	path: "js/json/data-navigationArrow-left.json",
+})
+
+
+
+// -----------------------------------------------------------------
+
+// const rollOutArrow = (arrow) =>{
+// 	arrow.playSegments([1,12]);
+// }
+
+// const rollInArrow = (arrow) =>{
+// 	arrow.playSegments([13,24]);
+// }
+
+// -----------------------------------------------------
+
+let isMousedOverRight = false;
+rightArrow = document.querySelector('.js-slick-next');
+rightArrow.addEventListener('mouseenter', ()=>{
+	console.log("mouseouver arrow")
+	// if(isMousedOverRight){
+	// 	return;
+	// }
+	// rollOutArrow(rightArrow);
+	rightArrowAnim.playSegments([1,12]);
+	isMousedOverRight = true;
+})
+rightArrow.addEventListener('mouseleave', ()=>{
+	console.log("mouse out arrow")
+	// rollInArrow(rightArrow);
+	rightArrowAnim.playSegments([13,24]);
+	isMousedOverRight = false;
+})
+
+let isMousedOverLeft = false;
+leftArrow = document.querySelector('.js-slick-prev');
+leftArrow.addEventListener('mouseenter', ()=>{
+	console.log("mouseover arrow")
+	// if(isMousedOverLeft){
+	// 	return;
+	// }
+	// rollOutArrow(leftArrow);
+	leftArrowAnim.playSegments([1,12])
+	isMousedOverLeft = true;
+})
+leftArrow.addEventListener('mouseleave', ()=>{
+	console.log("mouse out arrow")
+	// rollInArrow(leftArrow);
+	leftArrowAnim.playSegments([13,24]);
+	isMousedOverLeft = false;
+})
+
+
+
+
+// load Animations for character reaction
+const animation = bodymovin.loadAnimation({
+  container: document.querySelector('.bodymovin'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: "js/json/data-idle.json",
+})
+
+const happyAnimation = bodymovin.loadAnimation({
+	container: document.querySelector('.bodymovin'),
+    renderer: 'svg',
+    loop: false,
+    prerender: true,
+    autoplay: true,
+    autoloadSegments: false,
+    path: 'js/json/data-happy.json'
+})
+
 //Begin general Function Setup
 
 const changeText = (element, newText) =>{
@@ -79,6 +174,35 @@ const setQuestionTwo = (value, elementClass) =>{
 
 }
 
+
+
+const playReaction = (value) =>{
+	console.log('in playReaction');
+	if(value === 'rate-one'){
+		return happyAnimation.play();	
+	}
+
+	else if(value === 'rate-two'){
+		return happyAnimation.play();
+	}
+
+	else if(value === 'rate-three'){
+		return happyAnimation.play();
+	}
+
+}
+
+// end Function Setup
+
+// Set up Event Listeners
+// const $nextButton = $('.js-slick-next');
+// const $prevButton = $('.js-slick-prev');
+// $nextButton.nextArrow('js-slick-next');
+// $prevButton.prevArrow('js-slick-prev');
+
+
+
+
 const listOfButtons = document.querySelectorAll('.rate-star');
 
 for(let i = 0; i <listOfButtons.length; i++){
@@ -92,6 +216,8 @@ for(let i = 0; i <listOfButtons.length; i++){
 		characterResponse(listOfButtons[i].getAttribute('value'), '.js-question-one');
 		checkedValue = findCheckedValue('rating');
 		setQuestionTwo(checkedValue, '.js-question-two');
+		playReaction(checkedValue);
+
 	})
 	listOfButtons[i].addEventListener('mouseout', () =>{
 		if (isAnythingChecked('rating')) {
@@ -102,6 +228,9 @@ for(let i = 0; i <listOfButtons.length; i++){
 	})
 
 }
+
+
+
 
 
 
